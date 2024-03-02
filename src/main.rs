a470ef2d1c005146568626cff4f161ea5d1acc63
aespuna <3659361+aespuna@users.noreply.github.com>
2024-03-02 10:10:01 +0100

    Just read and count the number of lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::time::{Duration, Instant};
+
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("data/measurements.txt").expect("file should be readable");
+    let reader = BufReader::new(file);
+
+    let start = Instant::now();
+    let mut counter = 0u32;
+    for _line in reader.lines() {
+        // let line = line.expect("line should be read");
+        counter += 1;
+    }
+
+    let duration = start.elapsed();
+    eprintln!("Total lines processed: {}", counter);
+    print_time(duration);
+}
+
+fn print_time(duration: Duration) {
+    let seconds = duration.as_secs();
+    let (minutes, seconds) = (seconds / 60, seconds % 60);
+    eprintln!("It took {}m{}.{}s", minutes, seconds, duration.subsec_millis())
 }
